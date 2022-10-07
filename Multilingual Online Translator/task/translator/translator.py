05673f7fb27ef6340c7f56ef90055f1be0402b4e
magjene <pdfjene@mail.ru>
2022-10-07 19:03:12 +0300

    Added DOC

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,52 +1,87 @@
 """
 Project: Multilingual Online Translator
-Stage 3/7: Translations
+Stage 4/7: All of them
 
 
 Description
-To get readable results, you need to clean and format all the data you get from the web. It's a good thing that Python is a rich programming language with powerful text formatting out of the box!
+Great job! You now have a basic translation app that works well. Wouldn’t it be great though to expand it and include all available languages? This will finally make our translator a multilingual one!
 
-To make the results extracted from the web page easily readable, let's separate sentences and translations by newlines and put titles in front of different sections of the output.
+The maximum number of languages our translator can support is 13. They are:
+
+Arabic
+German
+English
+Spanish
+French
+Hebrew
+Japanese
+Dutch
+Polish
+Portuguese
+Romanian
+Russian
+Turkish
+They should be enumerated in the program. A great idea is to present them with relevant numbers so that the user can choose the first as the original language and the second as a translation.
 
 Objectives
-At this stage, format the output of results in the following fashion:
+At this stage, your program should:
 
-Output the line ... Translations:; put the full name of the target language instead of ... (for example, English Translations).
-Output found translations, one per line. Make sure there are no commas or quotes, just the word (or the phrase). If there are more than 5 translations, leave only 5 of them to keep the results more compact. Or you can print all of them, it does not affect the testing.
-Output the line ... Examples; put the full name of the target language instead of ... .
-Output found examples of sentences, one sentence per line. Make sure there are no commas or quotes (apart from those that should be inside the sentence). First, output the sentence in the source language, then output its translation in the target language. Repeat this procedure for every found sentence pair. If there are more than 5 sentence pairs, you can leave only 5 of them for convenience. Or you can print all of them, it does not affect the testing.
+Output the welcoming message (let's update it a bit): Hello, welcome to the translator. Translator supports:
+Output an enumerated list of languages. The enumeration should start from 1. The order of languages should be exactly as in the list above.
+Take input (a number from the list) specifying the source language (the language from which the translation should be performed).
+Take input (a number from the list) specifying the target language (the language to which the translation should be performed).
+Take input specifying the word that should be translated.
+Output the results as in the previous stage. At this stage, you don't need to print 200 OK anymore.
+Tip 1: just place the listed languages into the URL depending on the user’s choice!
+
+Tip 2: Try to convert the input to lower case: it may cause an error if the user's input is in upper case or mixed.
+
 Example
 The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
 
-Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:
-> fr
+Hello, welcome to the translator. Translator supports:
+1. Arabic
+2. German
+3. English
+4. Spanish
+5. French
+6. Hebrew
+7. Japanese
+8. Dutch
+9. Polish
+10. Portuguese
+11. Romanian
+12. Russian
+13. Turkish
+Type the number of your language:
+> 3
+Type the number of language you want to translate to:
+> 4
 Type the word you want to translate:
 > hello
-You chose "fr" as a language to translate "hello".
-200 OK
 
-French Translations:
-bonjour
-allô
-ohé
-coucou
-salut
+Spanish Translations:
+hola
+buenos días
+qué tal
+saludo
+buen día
 
-French Examples:
-Well, hello, freedom fighters.
-Et bien, bonjour combattants de la liberté.
+Spanish Examples:
+Well, hello, Miss Anchor-liar.:
+Bien, hola, señorita presentadora de mentiras.
 
-Goodbye England and hello the Netherlands...
-Au revoir l'Angleterre et bonjour les Pays-Bas...
+He didn't introduce us, so hello.:
+No nos presentó, así que hola.
 
-Yes, hello. Jackson speaking.
-Oui, allô, Jackson à l'appareil.
+Well, hello, Prince Charming.:
+Vaya, hola, Príncipe Azul.
 
-Hello, hello, hello, hello.
-Allô, allô, allô, allô.
+In addition, fast delivery. hello Laura.:
+Además, la entrega rápida. hola Laura.
 
-And began appearing hello kitty games online.
-Et a commencé à apparaître bonjour Kitty jeux en ligne.
+L: Well, hello, my dear secretary.:
+A: Bien, hola, mi querida secretaria.
 """
 
 
