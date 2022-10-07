e697e2a5b2a2d306e616756c38a64b29fe007dc3
magjene <pdfjene@mail.ru>
2022-10-07 13:24:32 +0300

    Added DOC

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,68 +1,20 @@
 """
 Project: Multilingual Online Translator
-Stage 2/7: Over the internet
+Stage 3/7: Translations
 
-
-Theory
-User-Agent field in a request
-
-When you use requests library, you can pass headers arguments to the function get(). Headers are text data you send over HTTP that might contain information about a web browser or application you use to surf the web. Your program doesn't use any web browser as we usually do, but it still has to present itself as some kind of a browser to be able to get the web pages. For this purpose, there's a 'User-Agent' field that forms a request.
-
-headers = {'User-Agent': 'Mozilla/5.0'}
-page = requests.get(url, headers=headers)
-Think of it as a visit card that your program shows to the website before it can enter it. It introduces itself as a web browser to the website it's trying to get a page from; otherwise, the website might not allow the connection from something other than a browser.
-
-Selecting text data
-
-When you access a web page, an important question is how to get the data that you need in a readable form without HTML tags. The answer is CSS. You can access this text via CSS classes and tags. All you need is to go to the web page, open your browser’s Dev Tools, and find these elements in CSS code. You can get access to your browser’s DevTools in three different ways:
-
-Keyboard: Ctrl + Shift + I, except
-
-Internet Explorer and Edge: F12
-macOS: ⌘ + ⌥ + I
-Menu bar:
-
-Firefox: Menu ➤ Web Developer ➤ Toggle Tools, or Tools ➤ Web Developer ➤ Toggle Tools
-Chrome: More tools ➤ Developer tools
-Safari: Develop ➤ Show Web Inspector. If you can't see the Develop menu, go to Safari ➤ Preferences ➤ Advanced, and check the Show Develop menu in the menu bar checkbox.
-Opera: Developer ➤ Developer tools
-Context menu: Press-and-hold/right-click an item on a web page (Ctrl-click on the Mac), and choose the option Inspect Element from the context menu that appears (an added bonus: this method highlights the code of the element you right-clicked.)
-
-After you’re done with CSS, check it.
 
 Description
-At this stage, you'll be able to implement a real translator program! A great website called ReversoContext will help you to do that. ReversoContext is a multilingual translator tool that allows seeing original phrases that should be translated and their equivalents in other languages in contexts (example sentences). That's a very useful feature since the meaning of the word depends greatly on the context. Hence, when you see a context, it's easier for you to choose the right translation.
+To get readable results, you need to clean and format all the data you get from the web. It's a good thing that Python is a rich programming language with powerful text formatting out of the box!
 
-The goal of your program at this stage is to find translations and example sentences for a given word. The word can be either in French or in English, and the translation should be in the opposite language (that is, English or French, respectively).
-
-To understand how to do this, go to ReversoContext and type any word you want to translate. After receiving the result, pay attention to the address bar of your browser. You will see the URL, for example:
-
-https://context.reverso.net/translation/english-french/cheese
-
-Here you see the language-translation pair «English-French», which represents the direction of translation, meaning that the translation is from English to French and not the other way around. After the last backslash, you can see the word being translated.
-
-Your goal is to make your program act as if it visits the website for you. To make it happen, tell your program to generate the correct URL with the word you type, determine the translation direction, and send the URL to the website.
-
-After getting to the needed page, the program should extract the required data: translations and sentences with usage examples. In the screenshot below, translations are highlighted with blue, and sentences for the target language are presented in a list in the right column.
-
-
+To make the results extracted from the web page easily readable, let's separate sentences and translations by newlines and put titles in front of different sections of the output.
 
 Objectives
-At this stage, your program should:
+At this stage, format the output of results in the following fashion:
 
-Take an input specifying the target language (en if the user wants to translate from French into English, or fr if the user wants to translate from English into French).
-Take an input specifying the word that should be translated.
-Output the confirmation message in the format You chose "..." as a language to translate "...".
-Form a request and connect to ReversoContext.
-Check the HTTP status of the response of the website to your request. If the status code is 200, you are good to proceed! If not... Try again?
-Output the response of the website to your request (200) and OK message to show that the connection is successful (so, the entire line should be 200 OK).
-Output the line Translations.
-Output a list with translations of the given word in the target language: ['bonjour', 'salut'].
-Output a list with examples of sentences featuring the given word or any of its translations: ['Well, hello, freedom fighters.', 'Et bien, bonjour combattants de la liberté.']. Both the original versions of the sentences and their translations should be printed. You don't need to filter sentences in any way: just print all the sentences that ReversoContext output for the given language pair and the given word.
-Make sure you output exactly the sentences that ReversoContext shows initially on the page https://context.reverso.net/translation/{language_1}-{language_2}/{word}. Don't confound them with the sentences that the website shows when you click on the first translation equivalent. These sentences will not be accepted by tests.
-
-Also, please, make sure your program follows the described format of the output.
-
+Output the line ... Translations:; put the full name of the target language instead of ... (for example, English Translations).
+Output found translations, one per line. Make sure there are no commas or quotes, just the word (or the phrase). If there are more than 5 translations, leave only 5 of them to keep the results more compact. Or you can print all of them, it does not affect the testing.
+Output the line ... Examples; put the full name of the target language instead of ... .
+Output found examples of sentences, one sentence per line. Make sure there are no commas or quotes (apart from those that should be inside the sentence). First, output the sentence in the source language, then output its translation in the target language. Repeat this procedure for every found sentence pair. If there are more than 5 sentence pairs, you can leave only 5 of them for convenience. Or you can print all of them, it does not affect the testing.
 Example
 The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
 
@@ -72,10 +24,29 @@ Type the word you want to translate:
 > hello
 You chose "fr" as a language to translate "hello".
 200 OK
-Translations
-['bonjour', 'allô', 'ohé', 'coucou', 'salut', 'hello', 'bonsoir', "quelqu'un", 'bien le bonjour', 'Oh', 'Enchanté', 'saluer', 'ça', 'salue', 'Oui']
-['Well, hello, freedom fighters.', 'Et bien, bonjour combattants de la liberté.', 'Goodbye England and hello the Netherlands...', "Au revoir l'Angleterre et bonjour les Pays-Bas...", 'Yes, hello. Jackson speaking.', "Oui, allô, Jackson à l'appareil.", 'Hello, hello, hello, hello.', 'Allô, allô, allô, allô.', 'And began appearing hello kitty games online.', 'Et a commencé à apparaître bonjour Kitty jeux en ligne.', 'Tell him hello... and congratulations.', 'Je lui dis bonjour et je le félicite.', 'A special hello to everyone from YouTube Bibi.', 'Un bonjour spécial à tout le monde de la chaîne de beauté YouTube de bibi.', 'Yes, hello, Mr Teodoresco.', 'Oui, bonjour, M. Teodoresco.', 'Well hello, Milan and Eve.', 'Eh bien bonjour, Milan et Eve.', 'Well hello, welcome to the Tree House pond.', "Alors bonjour, bienvenue à l'étang de la Maison de l'arbre.", 'pink hello kitty seat 2,3 - Auto Outlet', 'rose bonjour 2,3 siège de minou - Auto Outlet', 'hello world PL/SQL procedure successfully completed. SQL', 'bonjour procédure monde PL / SQL terminée avec succès. SQL', '"Maido-san" means something like "hello" in Kanazawa dialect.', 'Maido-sans veut dire quelque chose comme bonjour dans le dialecte de Kanazawa.', 'So anyway, hello and goodbye.', 'Donc voilà, bonjour et au revoir.', 'You can hardly hear him saying hello.', "On l'entend à peine dire bonjour.", "Yes, hello. I'd like to blackmail the Prime Minister.", "Oui, bonjour, j'aimerais faire chanter le premier Ministre.", 'Well, please tell her hello for us.', 'Bien, dites lui bonjour de notre part.', 'Homie, I think someone is saying hello.', "Homer, je crois qu'on te dit bonjour.", 'Well, hello, Susan and welcome.', 'Bien, bonjour Susan et bienvenue.', 'Normally, one says "hello" only once.', 'Normalement, on dit bonjour une fois.']
-Here you can see the results that are almost readable, but there are a lot of quotes and commas. The next stage is all about the representation!
+
+French Translations:
+bonjour
+allô
+ohé
+coucou
+salut
+
+French Examples:
+Well, hello, freedom fighters.
+Et bien, bonjour combattants de la liberté.
+
+Goodbye England and hello the Netherlands...
+Au revoir l'Angleterre et bonjour les Pays-Bas...
+
+Yes, hello. Jackson speaking.
+Oui, allô, Jackson à l'appareil.
+
+Hello, hello, hello, hello.
+Allô, allô, allô, allô.
+
+And began appearing hello kitty games online.
+Et a commencé à apparaître bonjour Kitty jeux en ligne.
 """
 
 
